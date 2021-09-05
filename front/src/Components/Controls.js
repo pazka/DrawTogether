9dcbe115383ed3b3e58f24dad7e7aaf5fd6e4f9e
Alexandre WEISSER <alexandre.weisser@gmail.com>
2021-09-05 21:42:04 +0200

    feat : style css

--- front/src/Components/Controls.js
+++ front/src/Components/Controls.js
@@ -9,7 +9,8 @@ export default function LayerControls() {
     const loadRef = useRef()
     let room = useRoom("ok")
     const [activeLayerId, setActiveLayer] = useLayer()
-    const [uploadStatus,setUploadStatus] = useState(false)
+    const [uploadStatus, setUploadStatus] = useState(false)
+    const [backgroundUploadStatus, setBackgroundUploadStatus] = useState(false)
 
     function handleFileChange(e) {
         let layerImg = inputRef.current.files[0];
@@ -23,87 +24,101 @@ export default function LayerControls() {
             alert(err)
         })
     }
-    
-    function createNewName(){
+
+    function createNewName() {
         send(On.ui_newName, null)
     }
 
     function saveProject() {
-        download(getRoomName()+'.json',JSON.stringify(room))
+        download(getRoomName() + '.json', JSON.stringify(room))
     }
-    
-    function loadProject (){
-        if(uploadStatus){
+
+    function loadProject() {
+        if (uploadStatus) {
             setUploadStatus(false)
             return;
         }
-        
+
         if (!window.confirm("This will OVERWRITE the current project !"))
             return;
-        
+
         setUploadStatus(true)
     }
-    
-    function handleProjectLoad(){
+
+    function handleProjectLoad() {
         if (!window.confirm("This will OVERWRITE the current project !"))
             return;
-        
+
         let projectFile = loadRef.current.files[0];
 
-        if(projectFile.name.split('.')[1] !== 'json'){
+        if (projectFile.name.split('.')[1] !== 'json') {
             alert("Bad format")
             return;
         }
-        
-        readFile(projectFile,(data)=>{
+
+        readFile(projectFile, (data) => {
             setUploadStatus(false)
-           let newRoom = JSON.parse(data)
+            let newRoom = JSON.parse(data)
             newRoom.version = Number(room.version) + 1
             newRoom.id = room.id
-            postData(`/api/room/${room.id}/import`,newRoom).then(
+            postData(`/api/room/${room.id}/import`, newRoom).then(
                 res => console.log(res)
             )
         })
     }
-    
-    function handleNewProject(){
+
+    function handleNewProject() {
         var name = window.prompt("Name of new project : ")
         window.location.replace(name)
     }
 
     let debug = 0
     let debugInterval
-    function _debug(){
-        if(debugInterval) {
+
+    function _debug() {
+        if (debugInterval) {
             debug = 1000
             return;
         }
-        
-        debugInterval = setInterval(()=>{
-            let position = {x: 1000 + 200 * Math.sin(Date.now() / 2000), y: 1000 + 200 *Math.cos(Date.now() / 2000) }
+
+        debugInterval = setInterval(() => {
+            let position = {x: 1000 + 200 * Math.sin(Date.now() / 2000), y: 1000 + 200 * Math.cos(Date.now() / 2000)}
             send(On.snd_mouse, {name: 'DEBUG', ...position})
-            if(debug++ > 1000) {
+            if (debug++ > 1000) {
                 clearInterval(debugInterval)
                 debug = 0
             }
-        },50)
+        }, 50)
     }
-    
+
     return <div className={"layer-controls"}>
-        {process.env.NODE_ENV !== 'production' && <button onClick={_debug}> Debug</button>}
-        <button onClick={createNewName }> New Text</button>
-        <form>
-            <label htmlFor="layerImg">Background => </label>
-            <input ref={inputRef} type="file" name={"layerImg"} onChange={handleFileChange}/>
-        </form>
-        <button onClick={()=>saveProject()}>Save project</button>
-        
         <span>
-            <button onClick={()=>{loadProject()}}>{uploadStatus ? "Close Upload" : "Import a project"}</button>
+                {process.env.NODE_ENV !== 'production' && <button onClick={_debug}> Debug</button>}
+            <button onClick={createNewName}> New Text</button>
+        </span>
+        <span>
+            <button onClick={() => {
+                setBackgroundUploadStatus(!backgroundUploadStatus)
+            }}>{backgroundUploadStatus ? "Close background upload" : "Change Background"}</button>
+            {backgroundUploadStatus && <form>
+                <label htmlFor="layerImg"> New background : </label>
+                <input ref={inputRef} type="file" name={"layerImg"} onChange={handleFileChange}/>
+            </form>}
+        </span>
+        <span>
+            <button onClick={() => saveProject()}>Save project</button>
+            
+            <button onClick={() => {
+                setUploadStatus(!uploadStatus)
+            }}>{uploadStatus ? "Close Upload" : "Import a project"}</button>
             {uploadStatus && <form>
                 <input ref={loadRef} type="file" name={"projectFile"} onChange={handleProjectLoad}/>
             </form>}
         </span>
-        <button onClick={()=>handleNewProject()}>New project</button>
+
+        <span>
+            <button onClick={() => handleNewProject()}>New project</button>
+        </span>
+
     </div>
 }
